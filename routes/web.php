75ca72247e16254505cd08771520e68ac260ec9e
mohit mawar <mohitmawar123@gmail.com>
2021-10-18 21:33:21 +0530

    remove extra code

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,3 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-/************************              admin panel request              ************************/
-Route::get('admin/', 'Auth\AdminLoginController@showLoginForm')->name('adminLoginForm');
-Route::post('admin/login', 'Auth\AdminLoginController@login')->name('adminLogin');
-Route::post('admin/logout', 'Auth\AdminLoginController@logout')->name('adminLogout');
-Route::group(['middleware' => ['web', 'admin.auth:admin'], 'prefix' => 'admin'], function () {
-    Route::get('dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin-dashboard');
-});
-/************************              end admin panel request              ************************/
-
-
-
-
-/************************              User panel request              ************************/
-Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'user' ,'namespace' => 'user'], function () {
-    Route::get('dashboard', 'QuizController@index')->name('user-dashboard');
-    Route::get('start-quiz/{quiz}', 'QuizController@start')->name('start.quiz');
-    Route::post('quiz-submit/{quiz_result}', 'QuizController@submit')->name('start.submit');
-});
